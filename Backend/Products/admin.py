7f8243f5d66ecda5a9231f4aeb997d84848bba6a
LeonitTeacher <leonit.krasniqi@digitalschool.tech>
2023-12-06 20:04:23 +0100

    fourth commit

--- Backend/Products/admin.py
+++ Backend/Products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from Products.models import Product, Stock, Category,  Purchase
+
+admin.site.register(Product)
+
+admin.site.register(Stock)
+
+admin.site.register(Category)
+admin.site.register(Purchase)
 
 # Register your models here.
