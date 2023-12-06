7aa1734853300cd75b53b7d574bef4db05b59c5a
LeonitTeacher <leonit.krasniqi@digitalschool.tech>
2023-12-06 19:57:04 +0100

    secind commit

---
+++ Backend/Products/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # You can add other fields like image, SKU, etc.
+
+    def __str__(self):
+        return self.name
+
+class Stock(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.product.name} - Stock: {self.quantity}"
+    
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    # You might want to add fields like purchase date, total price, etc.
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name} - Quantity: {self.quantity}"
