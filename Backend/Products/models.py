6a1457af223a8ef78ffd0df254659669cf263bd2
Leonit654 <leonit654@gmail.com>
2023-12-18 20:21:58 +0100

    Add photo field to models

--- Backend/Products/models.py
+++ Backend/Products/models.py
@@ -13,7 +13,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # You can add other fields like image, SKU, etc.
+    product_image=models.ImageField(upload_to='./Products/product_image')
+    
 
     def __str__(self):
         return self.name
